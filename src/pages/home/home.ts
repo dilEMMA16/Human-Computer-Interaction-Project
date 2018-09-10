5522ab4680e237908c77e21405693c6a01f78abf
Emma Schreifels <emma.schreifels@gmail.com>
2018-09-09 23:35:37 -0500

    home page work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PalettePage} from "../palette/palette";
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,9 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  navigateToPalettePage(): void {
+   this.navCtrl.push(PalettePage);
+}
 
 }
