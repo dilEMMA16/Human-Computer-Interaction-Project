508b5e2a2239170f5e73c0ec424375f581e3c8d1
Emma Schreifels <emma.schreifels@gmail.com>
2018-09-10 22:54:09 -0500

    work with home and palette passing vars

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,35 @@ import { PalettePage} from "../palette/palette";
 })
 export class HomePage {
 
+
   constructor(public navCtrl: NavController) {
 
   }
 
-  navigateToPalettePage(): void {
-   this.navCtrl.push(PalettePage);
+  navigateToPalettePage(theme): void {
+
+    var palette1:string[];
+    var palette2:string[]
+    var palette3:string[]
+    var palette4:string[]
+
+    //based on what theme is = pass in palettes
+    if (theme == "assets/imgs/8.png") //natural
+    {
+      palette1 = ["#baa994","#968663","#5d667b","#617b66"];
+      palette2 = ["#7b6156","#e3d6cc","#848b8c","#c9beb5"];
+      palette3 = ["#918696","#7b6972","#727b63","#d3af86"];
+      palette4 = ["#617b66","#c9beb5","#d3af86","#7b6e58"];
+    }
+
+   this.navCtrl.push(PalettePage, {
+     'data' : theme,
+     'palette1' : palette1,
+      'palette2' : palette2,
+      'palette3' : palette3,
+      'palette4' : palette4
+
+   });
 }
 
 }
--- src/pages/palette/palette.ts
+++ src/pages/palette/palette.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
 @Component({
   selector: 'page-palette',
   templateUrl: 'palette.html'
 })
+
 export class PalettePage {
 
-  constructor(public navCtrl: NavController) {
+  theme: any;
+  p1: any;
+  p2: any;
+  p3: any;
+  p4: any;
 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.theme = navParams.get('data');
+    this.p1 = navParams.get('palette1');
+    this.p2 = navParams.get('palette2');
+    this.p3 = navParams.get('palette3');
+    this.p4 = navParams.get('palette4');
   }
+
+
+
+
+
+
+
 
 }
