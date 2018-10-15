63f9a672c567645fe8cc4d8db3b11013ad65deb4
Emma Schreifels <emma.schreifels@gmail.com>
2018-10-15 12:33:59 -0500

    putting stuff back together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PalettePage } from '../pages/palette/palette';
+import { CustomPalettePage } from '../pages/custompalette/custompalette';
+import { EmailPage } from '../pages/email/email';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PalettePage,
+    CustomPalettePage,
+    EmailPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PalettePage,
+    CustomPalettePage,
+    EmailPage
   ],
   providers: [
     StatusBar,
