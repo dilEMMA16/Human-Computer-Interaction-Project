f21b5dbffea51c9059c887419412ba9d229568f2
DESKTOP-KBPAFB2\Enjila <enjila.mishra@ndus.edu>
2018-11-12 18:21:02 -0600

    Added customfont

--- src/pages/customfont/customfont.ts
+++ src/pages/customfont/customfont.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
 import { CustomPalettePage} from "../custompalette/custompalette";
 import { SavedPage} from "../saved/saved";
+import { EmailPage} from "../email/email";
+import { FontPage} from "../font/font";
 import { EmailComposer } from '@ionic-native/email-composer';
 @Component({
   selector: 'page-customfont',
@@ -19,8 +21,8 @@ export class CustomFontPage {
   font2: any;
   font1code: any;
   font2code: any;
-  headerfonts: any;
-  bodyfonts: any;
+  headerfonts: any; headerfontisselected: any; 
+  bodyfonts: any; bodyfontisselected:any;
 
   constructor(public navCtrl: NavController,public navParams : NavParams) {
     this.color1 = navParams.get('color1');
@@ -30,6 +32,8 @@ export class CustomFontPage {
     this.theme = ""+navParams.get('data')+"";
     this.headerfonts = navParams.get('headerfonts');
     this.bodyfonts = navParams.get('bodyfonts');
+	this.headerfontisselected = false;
+	this.bodyfontisselected = false;
   }
 
   navigateToHomePage() {
@@ -50,5 +54,81 @@ export class CustomFontPage {
 
   }
 
+showheaderDiv():void{
+	let element: HTMLElement = document.getElementById("headerDiv");
+	if (element.classList.contains("opened")) { 
+      //remove classes
+      element.classList.remove("opened");
+	  element.style.display = "none";
+	}
+	else {
+		element.classList.add("opened");
+	  element.style.display = "block";
+	}
 
+}
+
+headerfontClicked(font):void{
+	let element: HTMLElement = document.getElementById("headerDiv");
+	element.style.display = "none";
+	this.headerfontisselected= true;
+	this.font1 = font[0];
+	this.font1code= font[1];
+	let element1: HTMLElement = document.getElementById("showclickedheaderDiv");
+	element1.style.fontFamily = font[1];
+	element1.style.display = "block";
+	element1.innerHTML= this.font1+"";
+	
+}
+
+bodyfontClicked(font):void{
+	let element: HTMLElement = document.getElementById("bodyDiv");
+	element.style.display = "none";
+	this.bodyfontisselected= true;
+	this.font2 = font[0];
+	this.font2code= font[1];
+	let element2: HTMLElement = document.getElementById("showclickedbodyDiv");
+	element2.style.fontFamily = font[1];
+	element2.style.display = "block";
+	element2.innerHTML= this.font2+"";
+
+	}
+
+
+
+showbodyDiv():void{
+	let element: HTMLElement = document.getElementById("bodyDiv");
+	if (element.classList.contains("opened")) { 
+      //remove classes
+      element.classList.remove("opened");
+	  element.style.display = "none";
+	}
+	else {
+		element.classList.add("opened");
+	  element.style.display = "block";
+	}
+	
+}
+
+confirm():void{
+	if (this.headerfontisselected && this.bodyfontisselected){
+	this.navCtrl.push(EmailPage, {
+      'color1':this.color1,
+      'color2':this.color2,
+      'color3': this.color3,
+      'color4': this.color4,
+      'data' : this.theme,
+      'font1':this.font1,
+      'font2':this.font2,
+      'font1code':this.font1code,
+      'font2code':this.font2code
+	
+    });
+	}
+	else{
+		let element2: HTMLElement = document.getElementById("confirmError");
+	element2.style.display = "block";
+	element2.innerHTML= "Please make sure you have both the header and body fonts selected!";
+	}
+}
 }
